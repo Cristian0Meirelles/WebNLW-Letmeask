6c282280d97032b75308805b341794b519e9e0f2
Cristian0Meirelles <cristianocmeirelles@gmail.com>
2021-06-24 10:00:20 -0300

    aula 3 done

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,25 @@
+//import { Button } from './components/Button'
+import{BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import { Home } from './Pages/Home';
+import { NewRoom } from './Pages/NewRoom';
+import { Room } from './Pages/Room';
+
+import { AuthContextProvider } from './contexts/AthContext';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
